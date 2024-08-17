e565877af7de8b2c89f3fcaa35ab03ae39b35d78
Milton Chino <miltonchinowrk@gmail.com>
2024-08-17 16:45:07 -0400

    gallery advance 1

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,27 +1,35 @@
-// import img1 from '5.png'
-// import img2 from '7.png'
-// import img3 from '8.png'
-// import img4 from '11.png'
+// import { MouseEvent } from "react";
+// import { lazy } from "react";
 
 type Props = {};
 
 function Gallery({}: Props) {
-  let src = "./../src/assets/1.png";
+  let src = [
+    "./../src/assets/gallery/1.png",
+    "./../src/assets/gallery/2.png",
+    "./../src/assets/gallery/3.png",
+    "./../src/assets/gallery/4.png",
+    "./../src/assets/gallery/5.png",
+    "./../src/assets/gallery/6.png",
+  ];
+
+  // const handleClick = (e: MouseEvent) => {
+  //   console.log(e);
+  //   e.movementX = 10;
+  // };
+  // const images = lazy(()=> import("./assets/1.png") );
+
   return (
     <div className="container">
       <div className="row align-items-center">
         <div className="col">
           <div className="card">
-            <img className="card-img-top" src={src} alt="Card image cap" />
+            <img className="card-img-top" src={src[0]} alt="Card image cap" />
           </div>
         </div>
         <div className="col">
           <div className="card">
-            <img
-              className="card-img-top"
-              src={"./../src/assets/2.png"}
-              alt="Card image cap"
-            />
+            <img className="card-img-top" src={src[1]} alt="Card image cap" />
           </div>
         </div>
         <div className="w-100"></div>
